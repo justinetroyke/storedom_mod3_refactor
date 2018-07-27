e446cefcc4ed7291e2e99ad016cf25a35c946c39
Justine <jjtroyke@gmail.com>
2018-07-27 11:16:09 -0600

    Successful call, moving to service

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,10 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
-    response = Faraday.get = "https://api.bestbuy.com/v1/stores?area=80202,25&apiKey=#{ENV['best_buy_app_key']}"
-    binding.pry
+    # response = Faraday.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&apiKey=#{ENV['best_buy_app_key']}")
+    # results = JSON.parse(response.body)
+    # binding.pry
+    redirect_to "/search"
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  resources :search 
   resources :items, only: [:index, :show]
   namespace :api do
     namespace :v1 do
